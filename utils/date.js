4f5a87de8d7c9f6c0f30475c9f697250e6d38900
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-09-06 03:49:01 -0300

    fix: export getToday in date.js for ES module

--- utils/date.js
+++ utils/date.js
@@ -1,5 +1,4 @@
-function getToday() {
-
+export function getToday() {
     const today = new Date()
     const mm = String(today.getMonth() + 1).padStart(2, '0')
     const dd = String(today.getDate()).padStart(2, '0')
