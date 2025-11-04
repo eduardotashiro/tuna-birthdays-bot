1bd6ed9b1eab0a97b23f34f10faa05771e1fc72c
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-11-04 02:16:04 -0300

    feat(bot) initialize server

--- src/bot.js
+++ src/bot.js
@@ -3,16 +3,27 @@ import dotenv from 'dotenv'
 import { getBirthdaysToday } from '../db/dbQueries.js'
 import cron from "node-cron"
 
+import pkg from '@slack/bolt'
+const { App } = pkg;
+
+import { homeTab } from './homeTab/index.js'
+
 dotenv.config()
 
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN
+})
+homeTab(app)
 
-const slackBotToken = process.env.SLACK_BOT_TOKEN
+
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
 const channelId = process.env.CHANNEL_ID
 
-const client = new WebClient(slackBotToken)
+const client = new WebClient(SLACK_BOT_TOKEN)
 
 
-cron.schedule( '30 6 * * *', async () => {  // Hr 06:30
+cron.schedule('30 6 * * *', async () => {
 
     const aniversariantes = await getBirthdaysToday()
 
@@ -39,7 +50,7 @@ async function sendBirthdayMessage(slack_id, full_name, lang) {
         if (lang === 'es') {
             await client.chat.postMessage({
                 channel: channelId,
-                text: `¡Hoy estamos de fiesta 🎂✨! 
+                text: `¡Hoy estamos de fiesta 🎂✨
 Feliz cumple <@${slack_id}> !!! Que este nuevo año te traiga salud, éxitos y mucha felicidad 🥳🎉🌵`
             })
             console.log(`Mensagem enviada para ${full_name}`)
@@ -48,8 +59,8 @@ Feliz cumple <@${slack_id}> !!! Que este nuevo año te traiga salud, éxitos y m
         else if (lang === 'pt') {
             await client.chat.postMessage({
                 channel: channelId,
-                text: `Hoje é dia de comemorar 🎂✨! 
-Parabens <@${slack_id}> !!! Que seu novo ciclo seja cheio de saúde, sucesso e muitas alegrias 🥳🎉🌵`
+                text: `Hoje é dia de comemorar 🎂✨
+Parabéns <@${slack_id}> !!! Que seu novo ciclo seja cheio de saúde, sucesso e muitas alegrias 🥳🎉🌵`
             })
             console.log(`Mensagem enviada para ${full_name}`)
         }
@@ -60,6 +71,8 @@ Parabens <@${slack_id}> !!! Que seu novo ciclo seja cheio de saúde, sucesso e m
 }
 
 
+await app.start(process.env.PORT);
+console.log("Tuna Birthday running");
 
 
 
@@ -102,51 +115,4 @@ Parabens <@${slack_id}> !!! Que seu novo ciclo seja cheio de saúde, sucesso e m
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://www.youtube.com/watch?v=zL19uMsnpSU&list=RDzL19uMsnpSU&start_radio=1
+//Aniversários são engraçados, comemoramos o tempo que passou, mas esquecemos de agradecer o tempo que ainda temos, agradecer a quem ?
