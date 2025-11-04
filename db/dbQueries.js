2baa709781f2e1e8d50a567c81ddd274b1f09923
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-11-04 01:51:35 -0300

    feat(db): add getBirthdaysMonth query

--- db/dbQueries.js
+++ db/dbQueries.js
@@ -1,6 +1,7 @@
 import { pool } from './db.js'
 import { getToday } from '../utils/date.js'
 
+//pega MM-DD para parabenizar o fulano
 export async function getBirthdaysToday() {
     const today = getToday()
 
@@ -18,3 +19,27 @@ export async function getBirthdaysToday() {
         return [] //fallback
     }
 }
+
+
+//pega mẽs para mostrar que o fulano esta de aniversario nesse mês
+export async function getBirthdaysMonth() {
+
+    const today = new Date()
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    
+
+    try {
+        const res = await pool.query(
+        `SELECT full_name, TO_CHAR(birthday,'DD') AS day
+        FROM slack_users
+        WHERE TO_CHAR(birthday, 'MM') = $1
+        ORDER BY TO_CHAR(birthday, 'DD')::int`,
+        [month]
+        )
+        return res.rows
+    } catch (err) {
+        console.error('Erro ao buscar aniversariantes do mês:', err)
+        return [] //fallback
+    }
+}
+
